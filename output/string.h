5a031c2fe5b4d0eecefca0152316b3340a554d1c
Aaryan Shukla <aaryanshukla@google.com>
2024-06-12 22:23:33 +0000

    fixed output error on stdc_string.yaml

--- output/string.h
+++ output/string.h
@@ -1,11 +1,11 @@
 #ifndef STRING_H
 #define STRING_H
 
-#define None
+#define NULL
 typedef SizeTType;
 
 void * memcpy(void *__restrict, const void *__restrict, size_t);
-void * memmove(void *, const void *, size_t6);
+void * memmove(void *, const void *, size_t);
 int memcmp(const void *, const void *, size_t);
 void * memchr(const void *, int, size_t);
 void * memset(void *, int, size_t);
