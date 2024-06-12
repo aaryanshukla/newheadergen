5a39083beae6b5aa1355c1777bc46e6cf6412fd8
Aaryan Shukla <aaryanshukla@google.com>
2024-06-12 23:21:47 +0000

    changed some comments for better user understanding

--- generating_headers.py
+++ generating_headers.py
@@ -50,11 +50,11 @@ def main(yaml_file):
     header, macros, types, enums, functions = parse_yaml(yaml_content)
     header_content = generate_header(header, macros, types, enums, functions)
 
-    # Ensure the output directory exists
+    # Make sure output folder works and exists if you are using it
     output_dir = os.path.join(os.path.dirname(__file__), 'output')
     os.makedirs(output_dir, exist_ok=True)
 
-    # Write the header file to the output directory
+    # This sends the header file to the output folder
     output_file = os.path.join(output_dir, header)
     with open(output_file, "w") as header_file:
         header_file.write(header_content)
