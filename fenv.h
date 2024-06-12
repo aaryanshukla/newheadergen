5a975ef34391a6029ebe0d43ae124af11a8be5dc
Aaryan Shukla <aaryanshukla@google.com>
2024-06-12 21:09:47 +0000

    added types utility and changed arguments to paremeters for json

---
+++ fenv.h
@@ -0,0 +1,31 @@
+#ifndef FENV_H
+#define FENV_H
+
+#define FE_DIVBYZERO
+#define FE_INEXACT
+#define FE_INVALID
+#define FE_OVERFLOW
+#define FE_UNDERFLOW
+#define FE_ALL_EXCEPT
+#define FE_DOWNWARD
+#define FE_TONEAREST
+#define FE_TOWARDZERO
+#define FE_DFL_ENV
+typedef fenv_t;
+typedef fexcept_t;
+
+int feclearexcept(int);
+int fetestexcept(int);
+int fetestexceptflag(const fexcept_t *, int);
+int feraiseexcept(int);
+int fesetround(int);
+int fegetround();
+int fegetenv(fenv_t *);
+int fesetenv(const fenv_t *);
+int fegetexceptflag(fexcept_t *, int);
+int fesetexcept(int);
+int fesetexceptflag(const fexcept_t *, int);
+int feholdexcept(fenv_t *);
+int feupdateenv(const fenv_t *);
+
+#endif // FENV_H
--- generating_headers.py
+++ generating_headers.py
@@ -3,48 +3,61 @@
 import yaml
 import argparse
 
-def parse_function_spec(yaml_content):
-    headers = []
+def parse_yaml(yaml_content):
+    header = yaml_content.get('header', 'unknown.h')
+    macros = yaml_content.get('macros', [])
+    types = yaml_content.get('types', [])
+    enums = yaml_content.get('enums', [])
     functions = yaml_content.get('functions', [])
-    for function in functions:
-        func_name = function['name']
-        return_type = function['return_type']
-        params = []
-        for param in function['parameters']:
-            qualifiers = " ".join(param.get('qualifiers', []))
-            param_str = "{} {}".format(param['type'], qualifiers).strip()
-            params.append(param_str)
-        headers.append((func_name, return_type, params))
-    return headers
+    return header, macros, types, enums, functions
 
-def generate_header(header_name, functions):
-    header_content = "#ifndef {}_H\n".format(header_name.upper().replace('.', '_'))
-    header_content += "#define {}_H\n\n".format(header_name.upper().replace('.', '_'))
-    for func_name, return_type, params in functions:
-        param_str = ", ".join(param.split()[0] for param in params)  # Ensure only the parameter types are included
-        header_content += "{} {}(\n    {}\n);\n\n".format(return_type, func_name, param_str)
-    header_content += "#endif // {}_H\n".format(header_name.upper().replace('.', '_'))
+def generate_header(header, macros, types, enums, functions):
+    header_guard = header.replace('.', '_').upper()
+    header_content = f"#ifndef {header_guard}\n"
+    header_content += f"#define {header_guard}\n\n"
+
+    for macro in macros:
+        header_content += f"#define {macro['name']}\n"
+
+    for type_ in types:
+        header_content += f"typedef {type_};\n"
+
+    header_content += "\n"
+
+    for func in functions:
+        return_type = func['return_type']
+        func_name = func['name']
+        params = []
+        for param in func['parameters']:
+            if isinstance(param, str):
+                params.append(param)
+            else:
+                type_ = param['type']
+                qualifiers = " ".join(param.get('qualifiers', []))
+                param_str = f"{type_} {qualifiers}".strip()
+                params.append(param_str)
+        params_str = ", ".join(params)
+        header_content += f"{return_type} {func_name}({params_str});\n"
+
+    header_content += f"\n#endif // {header_guard}\n"
     return header_content
 
 def main(yaml_file):
     with open(yaml_file, 'r') as f:
         yaml_content = yaml.safe_load(f)
     
-    header_name = yaml_content.get('header', 'default')
-    functions = parse_function_spec(yaml_content)
+    header, macros, types, enums, functions = parse_yaml(yaml_content)
+    header_content = generate_header(header, macros, types, enums, functions)
+
+    with open(header, "w") as header_file:
+        header_file.write(header_content)
     
-    header_content = generate_header(header_name, functions)
-    
-    header_file = "{}.h".format(header_name)
-    with open(header_file, "w") as f:
-        f.write(header_content)
-    
-    print("Generated header for:", header_name)
+    print(f"Generated header for {header}")
     print(header_content)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate header files from YAML")
-    parser.add_argument("yaml_file", help="Path to the YAML file containing function definitions")
+    parser.add_argument("yaml_file", help="Path to the YAML file containing header specification")
     args = parser.parse_args()
     
     main(args.yaml_file)
