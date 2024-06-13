18cd09f2a9419c3a3fe4e419d146601e3fd06de1
Rose Zhang <rosezhang@google.com>
2024-06-13 20:34:46 +0000

    added another type test case

--- type_test.py
+++ type_test.py
@@ -7,6 +7,26 @@ class TestType(unittest.TestCase):
         expected_output = "#include <llvm-libc-types/float_t.h>"
         self.assertEqual(str(type), expected_output)
 
+    def test_type_capitalized(self):
+        type = Type("OnceFlagType")
+        expected_output = "#include <llvm-libc-types/once_flag.h>"
+        self.assertEqual(str(type), expected_output)
+
+    def test_type_ttype(self):
+        type = Type("ThrdTType")
+        expected_output = "#include <llvm-libc-types/thrd_t.h>"
+        self.assertEqual(str(type), expected_output)
+
+    def test_type_many_words(self):
+        type = Type("CallOnceFuncType")
+        expected_output = "#include <llvm-libc-types/__call_once_func_t.h>"
+        self.assertEqual(str(type), expected_output)
+
+    def test_double_underscore(self):
+        type = Type("QSortRCompareT")
+        expected_output = "#include <llvm-libc-types/__qsortcompare_t.h>"
+        self.assertEqual(str(type), expected_output)
+
     def test_type_empty_name(self):
         type = Type("")
         expected_output = "#include "
